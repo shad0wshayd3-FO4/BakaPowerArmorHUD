94365e9010c8b498551456bc218d95e91d789c8c
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-20 23:32:59 -0700

    refactor: xmake migration

---
+++ src/Hooks/Hooks.h
@@ -0,0 +1,316 @@
+#pragma once
+
+#include "MCM/MCM.h"
+#include "Menus/PowerArmorConditionMenu.h"
+
+class Hooks
+{
+public:
+	static void Install()
+	{
+		hkGetPowerArmorShouldBeVisible<1402809, 0x1D>::Install();   // HUDActionPointMeter::CanPlaySoundEffects
+		hkGetPowerArmorShouldBeVisible<742970, 0x190>::Install();   // HUDActiveEffectsDisplay::UpdateDisplayObject
+		hkGetPowerArmorShouldBeVisible<756668, 0x5D9>::Install();   // HUDCompassMarker::SetCompassMarkerData
+		hkGetPowerArmorShouldBeVisible<259269, 0xB6>::Install();    // HUDMarkerData::GetMinorFrame
+		hkGetPowerArmorShouldBeVisible<1511766, 0x107>::Install();  // PowerArmorGeometry::UpdatePowerArmorHUD
+		hkSetPowerArmorMode<1187476, 0xE6B>::Install();             // PowerArmor::SwitchToPowerArmor
+		hkSetPowerArmorMode<1150710, 0x1E8>::Install();             // PowerArmor::SwitchFromPowerArmorFurnitureLoaded
+		hkQActorInPowerArmor<31293, 0x14C, false>::Install();       // HUDRadiationModel::CalcEnvDamage
+		hkQActorInPowerArmor<733718, 0xA8, false>::Install();       // HUDMenu::InitializeHUD
+		hkQActorInPowerArmorPAHC<34363, 0x2D>::Install();           // HUDMenuUtils::GetGameplayHUDColor
+		hkCanBeVisible<893789, 0x12>::Install();                    // HUDCompass::CanBeVisible
+		hkGetPowerArmorHUDColor<523665, 0x56>::Install();           // GameUIModel::SetGameColors
+	}
+
+	static void InstallPostLoad()
+	{
+		MCM::Settings::Update();
+		Menus::Register();
+
+		if (auto PlayerCharacter = RE::PlayerCharacter::GetSingleton())
+		{
+			if (auto EventSource =
+			        static_cast<
+						RE::BSTEventSource<
+							RE::ActorValueEvents::ActorValueChangedEvent>*>(
+						PlayerCharacter))
+			{
+				EventSource->RegisterSink(ActorValueChangedHandler::GetSingleton());
+			}
+		}
+	}
+
+private:
+	class detail
+	{
+	public:
+		static bool IsExempt()
+		{
+			if (!MCM::Settings::General::bEnable)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		template <class T>
+		static void Notify()
+		{
+			if (auto EventSource = T::GetEventSource())
+			{
+				auto eventData = T{};
+				EventSource->Notify(eventData);
+			}
+		}
+
+		template <class T>
+		static void Notify(bool a_value)
+		{
+			if (auto EventSource = T::GetEventSource())
+			{
+				auto eventData = T{ a_value };
+				EventSource->Notify(eventData);
+			}
+		}
+	};
+
+	template <std::uint64_t ID, std::ptrdiff_t OFF>
+	class hkGetPowerArmorShouldBeVisible
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::ID(ID), OFF };
+			auto& trampoline = F4SE::GetTrampoline();
+			_GetPowerArmorShouldBeVisible = trampoline.write_call<5>(target.address(), GetPowerArmorShouldBeVisible);
+		}
+
+	private:
+		static bool GetPowerArmorShouldBeVisible(
+			[[maybe_unused]] RE::PowerArmorGeometry* a_this)
+		{
+			if (detail::IsExempt())
+			{
+				return _GetPowerArmorShouldBeVisible(a_this);
+			}
+
+			return false;
+		}
+
+		inline static REL::Relocation<decltype(&GetPowerArmorShouldBeVisible)> _GetPowerArmorShouldBeVisible;
+	};
+
+	template <std::uint64_t ID, std::ptrdiff_t OFF>
+	class hkSetPowerArmorMode
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::ID(ID), OFF };
+			auto& trampoline = F4SE::GetTrampoline();
+			_SetPowerArmorMode = trampoline.write_call<5>(target.address(), SetPowerArmorMode);
+		}
+
+	private:
+		static void SetPowerArmorMode(
+			[[maybe_unused]] bool a_inPowerArmor)
+		{
+			if (detail::IsExempt())
+			{
+				return _SetPowerArmorMode(a_inPowerArmor);
+			}
+
+			if (MCM::Settings::General::bDisableColor)
+			{
+				return;
+			}
+
+			detail::Notify<RE::ColorUpdateEvent>();
+		}
+
+		inline static REL::Relocation<decltype(&SetPowerArmorMode)> _SetPowerArmorMode;
+	};
+
+	template <std::uint64_t ID, std::ptrdiff_t OFF, bool RETN>
+	class hkQActorInPowerArmor
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::ID(ID), OFF };
+			auto& trampoline = F4SE::GetTrampoline();
+			_QActorInPowerArmor = trampoline.write_call<5>(target.address(), QActorInPowerArmor);
+		}
+
+	private:
+		static bool QActorInPowerArmor(
+			[[maybe_unused]] RE::Actor* a_actor)
+		{
+			if (detail::IsExempt())
+			{
+				return _QActorInPowerArmor(a_actor);
+			}
+
+			return RETN;
+		}
+
+		inline static REL::Relocation<decltype(&hkQActorInPowerArmor::QActorInPowerArmor)> _QActorInPowerArmor;
+	};
+
+	template <std::uint64_t ID, std::ptrdiff_t OFF>
+	class hkQActorInPowerArmorPAHC
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::ID(ID), OFF };
+			auto& trampoline = F4SE::GetTrampoline();
+			_QActorInPowerArmor = trampoline.write_call<5>(target.address(), QActorInPowerArmor);
+		}
+
+	private:
+		static bool QActorInPowerArmor(
+			[[maybe_unused]] RE::Actor* a_actor)
+		{
+			if (detail::IsExempt())
+			{
+				return _QActorInPowerArmor(a_actor);
+			}
+
+			if (MCM::Settings::General::bDisableColor)
+			{
+				return _QActorInPowerArmor(a_actor);
+			}
+
+			if (!MCM::Settings::General::bDisablePAColor)
+			{
+				return _QActorInPowerArmor(a_actor);
+			}
+
+			return false;
+		}
+
+		inline static REL::Relocation<decltype(&QActorInPowerArmor)> _QActorInPowerArmor;
+	};
+
+	template <std::uint64_t ID, std::ptrdiff_t OFF>
+	class hkCanBeVisible
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::ID(ID), OFF };
+			auto& trampoline = F4SE::GetTrampoline();
+			_CanBeVisible = trampoline.write_call<5>(target.address(), CanBeVisible);
+		}
+
+	private:
+		static bool CanBeVisible(
+			[[maybe_unused]] void* a_this)
+		{
+			if (detail::IsExempt())
+			{
+				return _CanBeVisible(a_this);
+			}
+
+			auto bCanBeVisible = _CanBeVisible(a_this);
+			if (bCanBeVisible && RE::PowerArmor::PlayerInPowerArmor())
+			{
+				Menus::PowerArmorConditionMenu::ShowMenu();
+			}
+			else
+			{
+				Menus::PowerArmorConditionMenu::HideMenu();
+			}
+
+			return bCanBeVisible;
+		}
+
+		inline static REL::Relocation<decltype(&CanBeVisible)> _CanBeVisible;
+	};
+
+	template <std::uint64_t ID, std::ptrdiff_t OFF>
+	class hkGetPowerArmorHUDColor
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::ID(ID), OFF };
+			auto& trampoline = F4SE::GetTrampoline();
+			_GetPowerArmorHUDColor = trampoline.write_call<5>(target.address(), GetPowerArmorHUDColor);
+		}
+
+	private:
+		static RE::NiColor GetPowerArmorHUDColor()
+		{
+			if (detail::IsExempt())
+			{
+				return _GetPowerArmorHUDColor();
+			}
+
+			if (MCM::Settings::General::bDisableColor)
+			{
+				return _GetPowerArmorHUDColor();
+			}
+
+			if (!MCM::Settings::General::bDisablePAColor)
+			{
+				return _GetPowerArmorHUDColor();
+			}
+
+			return GetGameplayHUDColor();
+		}
+
+		static RE::NiColor GetGameplayHUDColor()
+		{
+			using func_t = decltype(&GetGameplayHUDColor);
+			REL::Relocation<func_t> func{ REL::ID(34363) };
+			return func();
+		}
+
+		inline static REL::Relocation<decltype(&GetPowerArmorHUDColor)> _GetPowerArmorHUDColor;
+	};
+
+	class ActorValueChangedHandler :
+		public RE::BSTEventSink<RE::ActorValueEvents::ActorValueChangedEvent>
+	{
+	public:
+		[[nodiscard]] static ActorValueChangedHandler* GetSingleton()
+		{
+			static ActorValueChangedHandler singleton;
+			return std::addressof(singleton);
+		}
+
+		virtual RE::BSEventNotifyControl ProcessEvent(
+			const RE::ActorValueEvents::ActorValueChangedEvent& a_event,
+			RE::BSTEventSource<RE::ActorValueEvents::ActorValueChangedEvent>*) override
+		{
+			if (detail::IsExempt())
+			{
+				return RE::BSEventNotifyControl::kContinue;
+			}
+
+			auto ActorValue = RE::ActorValue::GetSingleton();
+			if (!ActorValue)
+			{
+				return RE::BSEventNotifyControl::kContinue;
+			}
+
+			switch (a_event.actorValue.formID)
+			{
+			case 0x0000035C:  // PowerArmorBattery
+				if (!RE::PowerArmor::PlayerInPowerArmor())
+				{
+					detail::Notify<RE::PowerArmorLightData>(false);
+				}
+				break;
+
+			default:
+				break;
+			}
+
+			return RE::BSEventNotifyControl::kContinue;
+		}
+	};
+};
---
+++ src/Menus/PowerArmorConditionMenu.h
@@ -0,0 +1,204 @@
+#pragma once
+
+#include "MCM/MCM.h"
+
+namespace Menus
+{
+	class PowerArmorConditionMenu :
+		public RE::GameMenuBase
+	{
+	public:
+		static constexpr auto MENU_NAME{ "PowerArmorConditionMenu"sv };
+
+		PowerArmorConditionMenu()
+		{
+			menuFlags.set(
+				RE::UI_MENU_FLAGS::kAllowSaving,
+				RE::UI_MENU_FLAGS::kDontHideCursorWhenTopmost,
+				RE::UI_MENU_FLAGS::kCustomRendering);
+			depthPriority.set(RE::UI_DEPTH_PRIORITY::kSWFLoader);
+
+			auto MoviePath = "Interface\\PowerArmorConditionMenu.swf"sv;
+			const auto ScaleformManager = RE::BSScaleformManager::GetSingleton();
+			[[maybe_unused]] const auto LoadMovieSuccess =
+				ScaleformManager->LoadMovieEx(*this, MoviePath, "root");
+			assert(LoadMovieSuccess);
+
+			filterHolder = RE::msvc::make_unique<RE::BSGFxShaderFXTarget>(*uiMovie, "root.Menu_mc");
+			if (filterHolder)
+			{
+				shaderFXObjects.push_back(filterHolder.get());
+			}
+
+			ConditionMeter_mc = RE::msvc::make_unique<RE::BSGFxShaderFXTarget>(*filterHolder, "ConditionMeter_mc");
+			if (ConditionMeter_mc)
+			{
+				ConditionMeter_mc->CreateAndSetFiltersToHUD(RE::HUDColorTypes::kPlayerSetColor);
+				shaderFXObjects.push_back(ConditionMeter_mc.get());
+			}
+		}
+
+		virtual ~PowerArmorConditionMenu()
+		{
+			ConditionMeter_mc.release();
+		}
+
+		static RE::IMenu* Create(const RE::UIMessage&)
+		{
+			return new PowerArmorConditionMenu();
+		}
+
+		static void ShowMenu()
+		{
+			if (!RE::PowerArmor::PlayerInPowerArmor())
+			{
+				return;
+			}
+
+			if (auto UI = RE::UI::GetSingleton())
+			{
+				if (UI->GetMenuOpen<PowerArmorConditionMenu>())
+				{
+					SetConditionMeterVisuals(
+						static_cast<float>(MCM::Settings::General::fConditionMeterX),
+						static_cast<float>(MCM::Settings::General::fConditionMeterY),
+						static_cast<float>(MCM::Settings::General::fConditionMeterScale));
+					UpdateBatteryState();
+					return;
+				}
+			}
+			else
+			{
+				return;
+			}
+
+			if (auto UIMessageQueue = RE::UIMessageQueue::GetSingleton())
+			{
+				UIMessageQueue->AddMessage(
+					"PowerArmorConditionMenu"sv,
+					RE::UI_MESSAGE_TYPE::kShow);
+			}
+		}
+
+		static void HideMenu()
+		{
+			if (auto UI = RE::UI::GetSingleton())
+			{
+				if (!UI->GetMenuOpen<PowerArmorConditionMenu>())
+				{
+					return;
+				}
+			}
+			else
+			{
+				return;
+			}
+
+			if (auto UIMessageQueue = RE::UIMessageQueue::GetSingleton())
+			{
+				UIMessageQueue->AddMessage(
+					"PowerArmorConditionMenu"sv,
+					RE::UI_MESSAGE_TYPE::kHide);
+			}
+		}
+
+		static void SetConditionMeterVisuals(float a_x, float a_y, float a_scale)
+		{
+			if (auto UI = RE::UI::GetSingleton())
+			{
+				if (auto Menu = UI->GetMenu<PowerArmorConditionMenu>())
+				{
+					Menu->SetConditionMeterVisualsImpl(a_x, a_y, a_scale);
+				}
+			}
+		}
+
+		static void UpdateBatteryState()
+		{
+			if (auto UI = RE::UI::GetSingleton())
+			{
+				if (auto Menu = UI->GetMenu<PowerArmorConditionMenu>())
+				{
+					Menu->UpdateBatteryStateImpl();
+				}
+			}
+		}
+
+	private:
+		class detail
+		{
+		public:
+			template <class T>
+			static void Notify(bool a_value)
+			{
+				if (auto EventSource = T::GetEventSource())
+				{
+					auto eventData = T{ a_value };
+					EventSource->Notify(eventData);
+				}
+			}
+		};
+
+		void SetConditionMeterCountImpl(std::uint32_t a_count)
+		{
+			RE::Scaleform::GFx::Value args[1];
+			args[0] = a_count;
+			ConditionMeter_mc->Invoke("SetCount", nullptr, args, 1);
+		}
+
+		void SetConditionMeterPercentImpl(float a_percent)
+		{
+			RE::Scaleform::GFx::Value args[1];
+			args[0] = a_percent;
+			ConditionMeter_mc->Invoke("SetPercent", nullptr, args, 1);
+			ConditionMeter_mc->SetToHUDColor(a_percent <= fPowerArmorLowBatterySoundThreshold->GetFloat());
+		}
+
+		void SetConditionMeterVisualsImpl(float a_x, float a_y, float a_scale)
+		{
+			ConditionMeter_mc->SetMember("x", a_x);
+			ConditionMeter_mc->SetMember("y", a_y);
+			ConditionMeter_mc->SetMember("scaleX", a_scale);
+			ConditionMeter_mc->SetMember("scaleY", a_scale);
+		}
+
+		void UpdateBatteryStateImpl()
+		{
+			if (auto PlayerCharacter = RE::PlayerCharacter::GetSingleton())
+			{
+				std::uint32_t count = PlayerCharacter->GetInventoryObjectCount(RE::PowerArmor::GetDefaultBatteryObject());
+				SetConditionMeterCountImpl(count);
+
+				if (auto ActorValue = RE::ActorValue::GetSingleton())
+				{
+					float value = PlayerCharacter->GetActorValue(*ActorValue->powerArmorBattery);
+					if (value <= 0.0f)
+					{
+						detail::Notify<RE::PowerArmorLightData>(true);
+					}
+					else
+					{
+						detail::Notify<RE::PowerArmorLightData>(false);
+					}
+
+					SetConditionMeterPercentImpl(value);
+				}
+			}
+		}
+
+		RE::msvc::unique_ptr<RE::BSGFxShaderFXTarget> ConditionMeter_mc;
+
+	protected:
+		inline static REL::Relocation<RE::SettingT<RE::GameSettingCollection>*> fPowerArmorLowBatterySoundThreshold{ REL::ID(370701) };
+	};
+
+	inline void Register()
+	{
+		if (auto UI = RE::UI::GetSingleton())
+		{
+			UI->RegisterMenu(
+				"PowerArmorConditionMenu",
+				PowerArmorConditionMenu::Create);
+		}
+	}
+}
